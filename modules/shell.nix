766ed827a584593a7b71312216c514cc226bd4d8
Miocasa <narimanabdualiev06@gmail.com>
2025-09-15 09:46:39 +0500

    oh my zsh and powerlevel10k configured

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,29 +1,17 @@
-# shell.nix
-
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ zsh-powerlevel10k ];
+{ pkgs, ... }:
+{
+  # Install zsh
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
     enableBashCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-    histSize = 10000;
-    shellAliases = {
-      #...
-    };
-    setOptions = [
-      "AUTO_CD"
-    ];
-    prompInit = {
-      "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-    };
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "dirhistory" "history" ];
-    };
   };
-  users.defaultUserShell = pkgs.zsh;
-  system.userActivationScripts.zshrc = "touch .zshrc";
-  environment.shells = with pkgs; [ zsh ];
+
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" "sudo" ];
+    custom = "$HOME/.oh-my-zsh/custom/";
+    theme = "robbyrussell";
+  };
 }
