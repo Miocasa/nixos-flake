61f4116fa6e6706fe303c13d68ab65ca8bbbb204
Miocasa <narimanabdualiev06@gmail.com>
2025-09-15 12:50:17 +0500

    homemanager integration

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS system flake with GNOME, GDM, Steam, Flatpak, NV/AMD drivers and dev tools";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
