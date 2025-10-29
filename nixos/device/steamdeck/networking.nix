57aca023f8a0e15b0fddd4b157d0d8b6cf4724e7
Miocasa <narimanabdualiev06@gmail.com>
2025-10-29 23:53:00 +0500

    hotfix, modules to import

--- nixos/device/steamdeck/networking.nix
+++ nixos/device/steamdeck/networking.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  networking.interfaces.enp1s0.ipv4.addresses = [ 
+  networking.interfaces.wlan0.ipv4.addresses = [ 
     # Format: "IP_ADDRESS/NETMASK_CIDR"
     { address = "192.168.0.45"; prefixLength = 24; } 
   ];
@@ -12,4 +12,20 @@
     "192.168.0.1" 
     "8.8.8.8" 
   ];
+  # networking = {
+  # useDHCP = false; # отключаем DHCP для всех интерфейсов
+  # wireless.enable = true;
+
+  # interfaces.wlan0 = {
+  #   ipv4.addresses = [
+  #     {
+  #       address = "192.168.0.40";
+  #       prefixLength = 24;
+  #     }
+  #   ];
+  # };
+
+  # defaultGateway = "192.168.0.1";
+  # nameservers = [ "192.168.0.1" "8.8.8.8" ];
+  # }
 }
