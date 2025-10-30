420b3dbac95c7ecc10762870001f3dbdfd1a3d13
Miocasa <narimanabdualiev06@gmail.com>
2025-10-30 08:43:46 +0500

    removing static ip

--- nixos/device/steamdeck/networking.nix
+++ nixos/device/steamdeck/networking.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  networking.interfaces.wlan0.ipv4.addresses = [ 
-    # Format: "IP_ADDRESS/NETMASK_CIDR"
-    { address = "192.168.0.45"; prefixLength = 24; } 
-  ];
+  # networking.interfaces.wlan0.ipv4.addresses = [ 
+  #   # Format: "IP_ADDRESS/NETMASK_CIDR"
+  #   { address = "192.168.0.45"; prefixLength = 24; } 
+  # ];
 
-  networking.defaultGateway = "192.168.0.1";
+  # networking.defaultGateway = "192.168.0.1";
 
-  networking.nameservers = [ 
-    "192.168.0.1" 
-    "8.8.8.8" 
-  ];
+  # networking.nameservers = [ 
+  #   "192.168.0.1" 
+  #   "8.8.8.8" 
+  # ];
   # networking = {
   # useDHCP = false; # отключаем DHCP для всех интерфейсов
   # wireless.enable = true;
